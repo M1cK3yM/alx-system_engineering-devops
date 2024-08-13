9bd5cc16c2ef3a6b28b82f896858a59973d8b77f
M1CK3YM <michaelyirdaw652@gmail.com>
2024-08-13 23:53:50 +0300

    change the user

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
         return 0
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced:v1.0.0 (by /u/firdaus_cartoon_jr)'}
+    headers = {'User-Agent': '0x16-api_advanced:v1.0.0 (by /u/Emergency-Cancel-250)'}
     r = requests.get(url, headers=headers).json()
     subs = r.get("data", {}).get("subscribers", 0)
     return subs
