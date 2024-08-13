13d47ccabf6cb14357ddb73b13f9a0538b963a09
M1CK3YM <michaelyirdaw652@gmail.com>
2024-08-13 23:25:40 +0300

    add 0-subs.py
    how many subs? determine the total number of subscribers of a give
    subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    
+    """returns the total number of subscribers"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced:v1.0.0 (by /u/firdaus_cartoon_jr)'}
+    r = requests.get(url, headers=headers).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
