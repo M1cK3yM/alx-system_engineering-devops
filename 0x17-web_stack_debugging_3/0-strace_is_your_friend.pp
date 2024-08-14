fa601c0800e9a3a32689aab64de58fda5d6743ce
M1CK3YM <michaelyirdaw652@gmail.com>
2024-08-15 01:09:28 +0300

    another fix on strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,6 @@
 exec { 'Fix wordpress site':
   command  => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-  provider => '/usr/local/bin/:/bin/'
+  path     => ['/usr/local/bin', '/bin', '/usr/bin'],
+  onlyif   => 'grep -q ".phpp" /var/www/html/wp-settings.php',
 }
+
