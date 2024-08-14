afb7cf1c55632ef262fc5c894fc52fd772a6a3e6
M1CK3YM <michaelyirdaw652@gmail.com>
2024-08-14 11:32:36 +0300

    PEP8 validation fix for 0-sub.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -4,14 +4,16 @@
 """
 import requests
 
+
 def number_of_subscribers(subreddit):
     """returns the total number of subscribers"""
     if subreddit is None or type(subreddit) is not str:
         return 0
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': '0x16-api_advanced:v1.0.0 (by /u/Emergency-Cancel-250)'}
+    headers = {'User-Agent': \
+               '0x16-api_advanced:v1.0.0 (by /u/Emergency-Cancel-250)'}
     r = requests.get(url, headers=headers).json()
     subs = r.get("data", {}).get("subscribers", 0)
     return subs
-
+ 
